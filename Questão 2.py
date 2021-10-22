c13c41c29d6007f7307c6e3c41c78cb388c70f2c
Maxwell Silva <83309150+maxwellssilva@users.noreply.github.com>
2021-10-22 10:55:02 -0400

    Update Questão 2.py

--- Questão 2.py
+++ Questão 2.py
@@ -1,4 +1,4 @@
 Dist = float(input("Qual a distância percorrida em KM? "))
 Comb = float(input("Qual a quantidade de combustível gasto em L? "))
 média = Dist/Comb
-print("O consumo médio entre {} e {} é {}km por litro" .format(Dist, Comb, média))
+print("O consumo médio entre {} e {} é {}km por litro".format(Dist, Comb, média))
